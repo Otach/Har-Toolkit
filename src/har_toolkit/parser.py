4ba91b665ace636b3277ec4d2e7c74f79549d4fd
Mason Weaver <mweaver1501@gmail.com>
2022-09-17 17:26:17 -0400

    Add Python Packaging for this Project

--- src/har_toolkit/parser.py
+++ src/har_toolkit/parser.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+# parser.py - A HAR (HTTP Archive) parser
+# Copyright (C) 2022  Mason Weaver <mason@swingproxy.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import json
 from base64 import b64decode
 from urllib.parse import urlparse
@@ -184,7 +201,7 @@ class Params:
         self.comment = data["comment"] if "comment" in data.keys() else None
 
 
-class Har:
+class HarReader:
 
     def __init__(self, filename):
 
